a67d44b99d827863e01b74e6b1f375e8084cb4e3
yck <2998770265@qq.com>
2024-10-14 19:57:01 +0800

    ch3

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -19,6 +19,9 @@ use crate::loader::{get_num_app, init_app_cx};
 use crate::sync::UPSafeCell;
 use lazy_static::*;
 use switch::__switch;
+use crate::syscall::TaskInfo;
+use crate::timer::get_time_ms;
+use crate::config::MAX_SYSCALL_NUM;
 pub use task::{TaskControlBlock, TaskStatus};
 
 pub use context::TaskContext;
@@ -53,11 +56,12 @@ lazy_static! {
         let num_app = get_num_app();
         let mut tasks = [TaskControlBlock {
             task_cx: TaskContext::zero_init(),
-            task_status: TaskStatus::UnInit,
+            task_info: TaskInfo::new(),
+            last_time: 0
         }; MAX_APP_NUM];
         for (i, task) in tasks.iter_mut().enumerate() {
             task.task_cx = TaskContext::goto_restore(init_app_cx(i));
-            task.task_status = TaskStatus::Ready;
+            task.task_info.status = TaskStatus::Ready;
         }
         TaskManager {
             num_app,
@@ -79,7 +83,8 @@ impl TaskManager {
     fn run_first_task(&self) -> ! {
         let mut inner = self.inner.exclusive_access();
         let task0 = &mut inner.tasks[0];
-        task0.task_status = TaskStatus::Running;
+        task0.task_info.status = TaskStatus::Running;
+        task0.last_time = get_time_ms();
         let next_task_cx_ptr = &task0.task_cx as *const TaskContext;
         drop(inner);
         let mut _unused = TaskContext::zero_init();
@@ -94,14 +99,14 @@ impl TaskManager {
     fn mark_current_suspended(&self) {
         let mut inner = self.inner.exclusive_access();
         let current = inner.current_task;
-        inner.tasks[current].task_status = TaskStatus::Ready;
+        inner.tasks[current].task_info.status = TaskStatus::Ready;
     }
 
     /// Change the status of current `Running` task into `Exited`.
     fn mark_current_exited(&self) {
         let mut inner = self.inner.exclusive_access();
         let current = inner.current_task;
-        inner.tasks[current].task_status = TaskStatus::Exited;
+        inner.tasks[current].task_info.status = TaskStatus::Exited;
     }
 
     /// Find next task to run and return task id.
@@ -112,7 +117,7 @@ impl TaskManager {
         let current = inner.current_task;
         (current + 1..current + self.num_app + 1)
             .map(|id| id % self.num_app)
-            .find(|id| inner.tasks[*id].task_status == TaskStatus::Ready)
+            .find(|id| inner.tasks[*id].task_info.status == TaskStatus::Ready)
     }
 
     /// Switch current `Running` task to the task we have found,
@@ -121,7 +126,10 @@ impl TaskManager {
         if let Some(next) = self.find_next_task() {
             let mut inner = self.inner.exclusive_access();
             let current = inner.current_task;
-            inner.tasks[next].task_status = TaskStatus::Running;
+            inner.tasks[next].task_info.status = TaskStatus::Running;
+            if inner.tasks[next].last_time == 0 {
+                inner.tasks[next].last_time = get_time_ms();
+            }
             inner.current_task = next;
             let current_task_cx_ptr = &mut inner.tasks[current].task_cx as *mut TaskContext;
             let next_task_cx_ptr = &inner.tasks[next].task_cx as *const TaskContext;
@@ -134,6 +142,23 @@ impl TaskManager {
         } else {
             panic!("All applications completed!");
         }
+    }
+
+    fn syscall_trace(&self, syscall_id: usize) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.tasks[current].task_info.syscall_times[syscall_id % MAX_SYSCALL_NUM] += 1;
+    }
+
+
+    fn fetch_task_info(&self) -> TaskInfo {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        let time = get_time_ms();
+
+        inner.tasks[current].task_info.time += time - inner.tasks[current].last_time;
+        inner.tasks[current].last_time = time;
+        inner.tasks[current].task_info
     }
 }
 
@@ -168,4 +193,14 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+
+/// syscall trace
+pub fn trace_syscall(syscall_id: usize) {
+    TASK_MANAGER.syscall_trace(syscall_id);
+}
+
+/// Fetch current task information
+pub fn fetch_task_info() -> TaskInfo {
+    TASK_MANAGER.fetch_task_info()
 }
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,14 +1,17 @@
 //! Types related to task management
 
 use super::TaskContext;
+use crate::syscall::TaskInfo;
 
 /// The task control block (TCB) of a task.
 #[derive(Copy, Clone)]
 pub struct TaskControlBlock {
     /// The task status in it's lifecycle
-    pub task_status: TaskStatus,
+    pub task_info: TaskInfo,
     /// The task context
     pub task_cx: TaskContext,
+    /// The time of last context switch
+    pub last_time: usize,
 }
 
 /// The status of a task
